4e795dac9dab8a15323c9b7c4bd6fd770c953033
adambourhim <benbourhim7@gmail.com>
2021-07-08 13:03:11 +0300

    cd

--- src/component/home/home.js
+++ src/component/home/home.js
@@ -4,8 +4,10 @@ import homeStyle from "./homeStyle.module.css";
 function home() {
   return (
     <div className={homeStyle.container}>
+      <div className={homeStyle.text}>
+        <p className={homeStyle.first}>Welcome</p>
+      </div>
       <img src={Gapriel} alt="gapriel" />
-      <p>Welcome</p>
     </div>
   );
 }
